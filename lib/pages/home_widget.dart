f6b3dd74e28f7acbe2968605ee3426d20ff06d03
Abdul Basit Ansari <abdul.basit@markitech.ca>
2021-03-30 18:27:21 +0500

    bottom navigation bar issue resolve

--- lib/pages/home_widget.dart
+++ lib/pages/home_widget.dart
@@ -12,8 +12,10 @@ class Home extends StatefulWidget {
   }
 }
 
-class _HomeState extends State<Home> with TickerProviderStateMixin {
-  TabController tabController;
+class _HomeState extends State<Home>
+// with TickerProviderStateMixin
+{
+  // TabController tabController;
   int _currentIndex = 0;
   final List<Widget> _tablist = [
     HomePage(),
@@ -26,44 +28,47 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     // PastTripsPage(),
     // PastTripsPage(),
   ];
-  @override
-  void initState() {
-    super.initState();
-    tabController =
-        TabController(initialIndex: 0, length: _tablist.length, vsync: this);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   tabController =
+  //       TabController(initialIndex: 0, length: _tablist.length, vsync: this);
+  // }
 
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   tabController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: tabController,
-        children: _tablist,
-      ),
+      body: _tablist[_currentIndex],
+      //  TabBarView(
+      //   controller: tabController,
+      //   children: _tablist,
+      // ),
       bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Color.fromRGBO(21, 183, 98, 1),
           type: BottomNavigationBarType.fixed,
-          showSelectedLabels: true,
+          // showSelectedLabels: true,
           currentIndex: _currentIndex,
-          onTap: (currentIndex) {
-            setState(() {
-              _currentIndex = currentIndex;
-            });
-            tabController.animateTo(_currentIndex);
-          },
+          onTap: onTabTapped,
+
+          // (currentIndex) {
+          //   setState(() {
+          //     _currentIndex = currentIndex;
+          //   });
+          //   tabController.animateTo(_currentIndex);
+          // },
           fixedColor: Colors.white,
           items: [
             BottomNavigationBarItem(
               backgroundColor: Colors.blue,
               icon: ImageIcon(
                 AssetImage('assets/Home.png'),
-                color: Colors.white,
+                // color: Colors.white,
               ),
               // ignore: deprecated_member_use
               label: '_____',
@@ -72,7 +77,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
               backgroundColor: Colors.blue,
               icon: ImageIcon(
                 AssetImage('assets/list.png'),
-                color: Colors.white,
+                // color: Colors.white,
               ),
               label: '_____',
             ),
@@ -80,7 +85,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
               backgroundColor: Colors.blue,
               icon: ImageIcon(
                 AssetImage('assets/mail.png'),
-                color: Colors.white,
+                // color: Colors.white,
               ),
               label: '_____',
             ),
@@ -88,7 +93,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
               backgroundColor: Colors.blue,
               icon: ImageIcon(
                 AssetImage('assets/Person.png'),
-                color: Colors.white,
+                // color: Colors.white,
               ),
               label: '_____',
             ),
